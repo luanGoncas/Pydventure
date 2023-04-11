e923e005dc7263879b0217849c5a950bb7db7680
Luan Santos Gonçalves <luansantos95@hotmail.com>
2023-04-10 23:00:20 -0300

    cutscenes - introduction | stage 01

--- Source/Cutscenes/FirstMission.gd
+++ Source/Cutscenes/FirstMission.gd
@@ -4,5 +4,5 @@ func _ready():
 	$VBoxContainer/VBoxContainer/Next.grab_focus()
 
 func _on_Next_pressed():
-	print("YEAH! I DIED!!!")
+	print("YEAH! TEST!!!")
 	SceneChanger.change_scene("res://Source/Levels/Level_01.tscn")
